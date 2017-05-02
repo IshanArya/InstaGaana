b11448c3cb442a230f50f733bb67bbce2672b77f
Sumit Dhingra <LinuxSDA@gmail.com>
2017-05-02 21:52:54 +0530

    Entry Points

--- setup.py
+++ setup.py
@@ -2,17 +2,24 @@ from setuptools import setup
 
 
 setup(name='InstaGaana',
-      version='1.02',
+      version='1.1',
       description='Instant music downlaoder for Saavn',
       url='https://github.com/LinuxSDA',
       author='Sumit Dhingra',
       author_email='LinuxSDA@gmail.com',
       license='MIT',
-      scripts=['InstaGaana'],
+      packages=['InstaGaana'],
       install_requires=[
           'BeautifulSoup4',
           'eyed3',
           'requests',
           'urllib3',
           'wget',
-      ])
+      ],
+      entry_points={
+        "console_scripts": [
+            "InstaGaana = InstaGaana.InstaGaana:main"
+        ]
+      },
+
+      )
